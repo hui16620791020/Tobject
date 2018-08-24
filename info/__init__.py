ebaf0aa4139af84d1b38669b3a4317b9782c8cd7
279752917@qq.com <XIAOxiaozicq520>
2018-08-24 11:17:52 +0800

    抽取config.py & 抽取app工厂方法 & 日志文件的配置

---
+++ info/__init__.py
@@ -0,0 +1,56 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from redis import StrictRedis
+from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
+from config import config_dict
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+def create_log(config_name):
+    """记录日志的配置函数"""
+    # 设置日志的记录等级
+    # config_dict[config_name].LOG_LEVEL 获取开发环境日志级别
+    logging.basicConfig(level=config_dict[config_name].LOG_LEVEL)  # 调试debug级
+
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 *100, backupCount=10)
+
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    # INFO manage.py 18 错误信息
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
+
+# 外界使用方式:  create_app("development") --> 开发模式的app
+#              create_app("production")  --> 线上模式的app
+
+def create_app(config_name): # development
+    """生产app的工厂方法"""
+
+    # 0.记录日志
+    create_log(config_name)
+    # 1.创建app对象
+    app = Flask(__name__)
+    # 2.注册配置信息到app对象
+    # config_dict["development"]: DevelopmentConfig 开发模式的配置
+    configClass = config_dict[config_name]
+    app.config.from_object(configClass)
+
+    # 3. 创建mysql数据库对象
+    db = SQLAlchemy(app)
+    # 4. 创建redis数据库对象
+    redis_store = StrictRedis(host=configClass.REDIS_HOST, port=configClass.REDIS_PORT,
+                              db=configClass.REDIS_NUM)
+    # 5. 开启csrf后端保护验证机制
+    # 提取cookie中的csrf_token和ajax请求头里面csrf_token进行比较验证操作
+    csrf = CSRFProtect(app)
+    # 6.创建session拓展类的对象(将session的存储调整到redis中)
+    Session(app)
+
+    return app
--- manage.py
+++ manage.py
@@ -1,58 +1,15 @@
-from flask import Flask, session
-from flask_sqlalchemy import SQLAlchemy
-from redis import StrictRedis
-from flask_wtf.csrf import CSRFProtect
-from flask_session import Session
+from flask import Flask, session, current_app
+from flask_script import Manager
+from info import create_app
+import logging
 
 
-# 创建项目配置类
-class Config(object):
-    """项目配置类"""
+# 单一职责的原则：manage.py 仅仅作为项目启动文件即可
+# 工厂方法的调用（ofo公司）
+app = create_app("development")
 
-    # 开启debug模式
-    DEBUG = True
-
-    # mysql数据库配置
-    #数据库链接配置
-    SQLALCHEMY_DATABASE_URI = "mysql://root:cq@127.0.0.1:3306/information16"
-    # 不跟踪数据库修改
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # redis数据库配置信息
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-    REDIS_NUM = 6
-
-    # 设置加密字符串
-    SECRET_KEY = "/W73UULUS4UFO5omviuVZz6+Bcjs5+2nRdvmyYNq1wEryZsMeluALSDGxGnuYoKX"
-    # 调整session存储位置(存储到redis)
-    # 指明sesion存储到那种类型的数据库
-    SESSION_TYPE = "redis"
-    # 上面的指明的数据库的实例对象
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT,db=REDIS_NUM)
-    # session数据需要加密
-    SESSION_USE_SIGNER = True
-    # 不设置永久存储
-    SESSION_PERMANENT = False
-    # 默认存储的有效时长 （没有调整之前默认值是timedelta(days=31)）
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
-
-
-
-
-# 1.创建app对象
-app = Flask(__name__)
-# 2.注册配置信息到app对象
-app.config.from_object(Config)
-# 3. 创建mysql数据库对象
-db = SQLAlchemy(app)
-# 4. 创建redis数据库对象
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=Config.REDIS_NUM)
-# 5. 开启csrf后端保护验证机制
-# 提取cookie中的csrf_token和ajax请求头里面csrf_token进行比较验证操作
-csrf = CSRFProtect(app)
-# 6.创建session拓展类的对象(将session的存储调整到redis中)
-Session(app)
+# 7. 创建manager管理类
+manager = Manager(app)
 
 
 
@@ -66,4 +23,16 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # python manage.py runserver -h -p -d
+
+    logging.debug("debug的日志信息")
+    logging.info("info的日志信息")
+    logging.warning("warning的日志信息")
+    logging.error("error的日志信息")
+    logging.critical("critical的日志信息")
+
+    # 在 Flask框架 中，其自己对 Python 的 logging 进行了封装，在 Flask 应用程序中，可以以如下方式进行输出 log:
+    current_app.logger.info("使用flask封住好的方法 info的日志信息")
+
+
+    manager.run()
