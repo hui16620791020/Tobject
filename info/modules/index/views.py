0f6a62834151b261d41d91c8baf217a70a50b02b
279752917@qq.com <XIAOxiaozicq520>
2018-08-27 12:45:49 +0800

    新闻首页后端逻辑实现

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,75 @@
 from info.models import User, News
+from info.utils.response_code import RET
 from . import index_bp
-from flask import session, current_app, render_template
+from flask import session, current_app, render_template, request, jsonify
 from info import redis_store, models, constants
+
+
+@index_bp.route('/news_list')
+def get_news_list():
+    """获取首页新闻列表的数据接口"""
+    """
+    1.获取参数
+        1.1 cid分类id page当前页码（默认展示第一页） per_page当前页展示多少条数据（默认展示10条新闻）
+    2.校验参数
+        2.1 非空判断
+        2.2 对数据类型进行校验判断
+    3.逻辑处理
+        3.3 根据上传参数进行数据库分页查询
+        3.4 将数据转换成dict对象给前端使用
+    4.返回值处理
+    """
+    #1.1 cid分类id page当前页码（默认展示第一页） per_page当前页展示多少条数据（默认展示10条新闻）
+    cid = request.args.get("cid")
+    page = request.args.get("page", 1)
+    per_page = request.args.get("per_page", constants.HOME_PAGE_MAX_NEWS)
+
+    #2.1 非空判断
+    if not all([cid, page, per_page]):
+        return jsonify(errno=RET.DATAERR, errmsg="参数不足")
+    #2.2 对数据类型进行校验判断
+    try:
+        cid = int(cid)
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DATAERR, errmsg="数据类型错误")
+
+    # 最新的新闻 不需要根据cid来查询只需要根据创建时间来查询
+    filters = []
+    if cid != 1:
+        # 将查询条件添加到filter列表中
+        filters.append(News.category_id == cid)
+
+    try:
+        #3.3 根据上传参数进行数据库分页查询
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询新闻列表数据异常")
+
+    # 当前页码所有数据
+    items = paginate.items
+    # 当前页码
+    current_page = paginate.page
+    # 总页数
+    total_page = paginate.pages
+
+    #3.4 将数据转换成dict对象给前端使用
+    newsList = []
+    # 遍历items模型列表获取个每一个模型进而转换成字典添加在newsList中
+    for news in items if items else None:
+        newsList.append(news.to_dict())
+    # 组织数据
+    data = {
+        "newsList": newsList,
+        "current_page": current_page,
+        "total_page": total_page
+    }
+    #4.组织响应对象
+    return jsonify(errno=RET.OK, errmsg="查询新闻list数据成功", data=data)
+
 
 
 #2. 使用蓝图对象
