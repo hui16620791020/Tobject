556a686cc5f2c40b64e85a446fcc878367b350dd
279752917@qq.com <XIAOxiaozicq520>
2018-08-30 12:55:38 +0800

    修改用户头像后端接口

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -2,12 +2,61 @@ from flask import current_app
 from flask import g, jsonify
 from flask import request
 from flask import session
-
 from info import db
 from info.utils.response_code import RET
 from . import profile_bp
 from flask import render_template
 from info.utils.common import login_user_data
+from info.utils.image_store import qiniu_image_store
+from info import constants
+
+# /user/pic_info  --> GET
+@profile_bp.route('/pic_info', methods=["GET", "POST"])
+@login_user_data
+def pic_info():
+    """修改用户头像"""
+    # 获取用户对象
+    user = g.user
+
+    if request.method == "GET":
+        return render_template("news/user_pic_info.html")
+
+    #POST 获取用户提交的头像数据进行修改
+    # 1.获取参数
+    avatar_data = request.files.get("avatar").read()
+    # 2.参数校验
+    # 2.1 判断否为空
+    if not avatar_data:
+        return jsonify(errno=RET.NODATA, errmsg="请上传用户头像")
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+    #3. 逻辑处理
+    try:
+        image_name = qiniu_image_store(avatar_data)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg="七牛云上传图片失败")
+
+    # 保存图片名称到user对象的avatar_url属性中
+    user.avatar_url = image_name # url域名 + image_name (方便后期更改域名)
+
+    # 保存修改操作到数据库
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="保存图片数据异常")
+
+    #4.返回值处理
+    full_url = constants.QINIU_DOMIN_PREFIX + image_name
+    data = {
+        "avatar_url": full_url
+    }
+    return jsonify(errno=RET.OK, errmsg="修改用户头像成功", data=data)
+
+
+
 
 
 # /user/user_info  --> GET
